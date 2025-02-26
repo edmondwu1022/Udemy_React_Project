7fbbfc9ed9df5a234e84d5dd4aaf0138ee067d9a
unknown <edmondwu1022@gmail.com>
2025-02-26 14:55:27 +0800

    Increase React-Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router"
+import Home from './pages/Home'
+import Offers from './pages/Offers'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import ForgotPassword from './pages/ForgotPassword'
+
 export default function App() {
   return <>
-    <h1 className="text-2xl underline mt-0 ml-0 bg-blue-300">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} hand />
+        <Route path="/Offers" element={<Offers />} hand />
+        <Route path="/Profile" element={<Profile />} hand />
+        <Route path="/Sign-In" element={<SignIn />} hand />
+        <Route path="/Sign-Up" element={<SignUp />} hand />
+        <Route path="/Forgot-Password" element={<ForgotPassword />} hand />
+      </Routes>
+    </Router>
   </>
 }
