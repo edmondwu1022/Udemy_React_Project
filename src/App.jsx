8e1ebd27963175a819b2aea1f6591e8124fe023f
unknown <edmondwu1022@gmail.com>
2025-02-27 17:25:38 +0800

    add header and use react router

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
+import Header from "./components/Header"
 
 export default function App() {
   return <>
     <Router>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} hand />
-        <Route path="/Offers" element={<Offers />} hand />
-        <Route path="/Profile" element={<Profile />} hand />
-        <Route path="/Sign-In" element={<SignIn />} hand />
-        <Route path="/Sign-Up" element={<SignUp />} hand />
-        <Route path="/Forgot-Password" element={<ForgotPassword />} hand />
+        <Route path="/offers" element={<Offers />} hand />
+        <Route path="/profile" element={<Profile />} hand />
+        <Route path="/sign-in" element={<SignIn />} hand />
+        <Route path="/sign-up" element={<SignUp />} hand />
+        <Route path="/forgot-password" element={<ForgotPassword />} hand />
       </Routes>
     </Router>
   </>
---
+++ src/components/Header.jsx
@@ -0,0 +1,43 @@
+import { useLocation, useNavigate } from "react-router"
+
+export default function Header() {
+    const location = useLocation()
+    const navigation = useNavigate()
+
+    function isPathMatch(path) {
+        if (path === location.pathname)
+            return true
+    }
+    return (
+        <div className=" bg-white border-b-1 shadow-sm sticky top-0 z-50" >
+            <header className="flex justify-between items-center h-15 px-6 max-w-5/6 mx-auto ">
+                <div>
+                    <img src="https://godotengine.org/assets/logo_dark.svg"
+                        alt="Logo"
+                        className="cursor-pointer h-10 ml-5"
+                        onClick={() => navigation("/")}
+                    />
+                </div>
+                <div>
+                    <ul className="flex justify-end items-center space-x-10 mr-5">
+                        <li className={
+                            `${(isPathMatch("/") ? " text-black border-b-red-500" : "text-gray-400")}
+                            cursor-pointer py-3 text-sm font-semibold border-b-3 border-transparent`}
+                            onClick={() => navigation('/')}
+                        >Home</li>
+                        <li className={
+                            `${(isPathMatch("/profile") ? " text-black border-b-red-500" : "text-gray-400")}
+                            cursor-pointer py-3 text-sm font-semibold border-b-3 border-transparent`}
+                            onClick={() => navigation('/profile')}
+                        >Profile</li>
+                        <li className={
+                            `${(isPathMatch("/sign-up") ? " text-black border-b-red-500" : "text-gray-400")}
+                            cursor-pointer py-3 text-sm font-semibold border-b-3 border-transparent`}
+                            onClick={() => navigation("/sign-up")}
+                        >Sing Up</li>
+                    </ul>
+                </div>
+            </header>
+        </div>
+    )
+}
