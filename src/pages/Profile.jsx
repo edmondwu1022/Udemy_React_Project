feb62e4bdaabdaa09a6fe627958e350a2c7d091e
unknown <edmondwu1022@gmail.com>
2025-03-10 18:02:48 +0800

    starting profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,5 +1,28 @@
+import { useState } from "react"
+
 export default function Profile() {
+    const [formData, setFormData] = useState({
+        name: "Edmond",
+        email: ""
+    })
+    const { name, email } = formData
     return (
-        <div>Profile</div>
+        <>
+            <section className="flex justify-self-center flex-col">
+                <h1 className="text-3xl text-center font-bold mt-6">My Profile</h1>
+                <div className="w-full">
+                    <form>
+                        {/* Name Input */}
+                        <input
+                            type="text"
+                            id="name"
+                            value={name}
+                            disabled
+                            className="sm:w-full py-2 px-4 mt-6 rounded-md text-gray-700 bg-white "
+                        ></input>
+                    </form>
+                </div>
+            </section>
+        </>
     )
 }
